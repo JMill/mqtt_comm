84dd1f55ab26b4a6055cbcbbb5daa14baf750087
JMill <Jonathan@JMillville.com>
2017-05-13 14:32:54 -0400

    Added stub for twin servo control via MQTT

---
+++ tessel/tessel_twinservo.js
@@ -0,0 +1,50 @@
+//To run on Twin Servo Tessel: listen for coordinates
+var tessel = require('tessel');
+var servolib = require('servo-pca9685');
+var mqtt = require('mqtt');
+
+var servo = servolib.use(tessel.port['A']);
+var servoY = 1; //x-axis
+var servoX = 16; //y-axis
+
+
+
+
+
+client = mqtt.connect('mqtt://162.243.219.88', 1883);
+
+client.on('connect',function(){
+	console.log('connected');
+	client.subscribe('ae/turret');
+});
+
+client.on('message', function(topic, message){
+	// message is Buffer
+	console.log(message.toString())
+	client.end()
+})
+
+
+var positionX = 0.4980; // 0.000 = LEFT
+var positionY = 0.0580; // 0.0000 = DOWN
+
+
+servo.on('ready', function () {
+	
+	//settings: 0.05, 0.12
+	servo.configure(servoX, 0.025, 0.123, function (){
+		setInterval(function () {
+			console.log('X:', positionX);
+			servo.move(servoX, positionX);
+		}, 1000); //1000 milliseconds
+	});
+
+	servo.configure(servoY, 0.026, 0.057, function (){
+		setInterval(function () {
+			console.log('Y:', positionY);
+			servo.move(servoY, positionY);
+		}, 1000); //1000 milliseconds
+	});
+
+	
+});
