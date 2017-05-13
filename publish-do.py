e90ea928d7133ef8b06226dc14d1e99d7e80d324
JMill <Jonathan@JMillville.com>
2017-05-13 17:25:39 -0400

    Broadcast random x, y coordinate to turret every specified seconds

--- publish-do.py
+++ publish-do.py
@@ -1,19 +1,29 @@
 import paho.mqtt.client as mqtt
+import json
+import random
+import time
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-	print ("Connected with result code "+str(rc))
+    print ("Connected with result code "+str(rc))
 
-	#Subscribing in on_connect() means that if we lose the connection and
-	#reconnect then subscriptions will be renewed
-	client.subscribe("device/#")
+    #Subscribing in on_connect() means that if we lose the connection and
+    #reconnect then subscriptions will be renewed
+    client.subscribe("device/#")
 
 client = mqtt.Client()
 client.on_connect = on_connect
 
 client.connect("162.243.219.88", 1883, 60)
 
-client.publish("device/hi", 'hi')
+while 1 > 0: 
+    payload = json.dumps({"position_x": str(random.random()), "position_y": str(random.random())})
+    print(payload)
+    # payload = json.loads(payload) # you can use json.loads to convert string to json
+    # print(payload)
+    client.publish("ae/turret", payload)
+    time.sleep(3) # in seconds
+
 
 #Blocking call that processes network traffic, dispatches callbacks and
 #handles reconnecting.
